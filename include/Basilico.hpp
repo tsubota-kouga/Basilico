b9981cd81f3e6b74a875b8d1c4467870e1ad6793
tsubota-kouga <kouga.infini@gmail.com>
2019-03-10 09:53:52 +0900

    add: default files

--- include/Basilico.hpp
+++ include/Basilico.hpp
@@ -5,26 +5,37 @@
 #include "BasilPlugin.hpp"
 
 
-#include "RuntimeView.hpp"
-
-#include "WebView.hpp"
-
-#include "PDFView.hpp"
-
 
 #include <QtWidgets>
 #include <QtGui>
-#include <type_traits>
+
+#include <set>
 
 class BasilPlugin;
 
-class RuntimeView;
-
-class WebView;
-
-class PDFView;
-
-
+// -Basilico---------------------------------------------------------
+// |-neovim_integrate(layout:basil_layout)--------------------------|
+// || -tabline(QTabBar)------------------------------------------- ||
+// || |NeoVim-tab TabPlugin-tab                                  | ||
+// || ------------------------------------------------------------ ||
+// || -neovimsplinteg_tabplugins_integrate(QStackedWidget)-------- ||
+// || |                                                          | ||
+// || |  -neovim_split_plugins_integrate------  -tabplugins---   | ||
+// || |  |   (layout:neovim_layout)          |  |            |-  | ||
+// || |  | -neovim(NeoVim)-- -splitplugins   |  |            ||  | ||
+// || |  | |               | |           |-  |  |            ||  | ||
+// || |  | |               | |           ||  |  |            ||  | ||
+// || |  | |               | |           ||  |  |            ||  | ||
+// || |  | |               | |           ||  |  |            ||  | ||
+// || |  | |               | |           ||  |  |            ||  | ||
+// || |  | |               | |           ||  |  |            ||  | ||
+// || |  | ----------------- -------------|  |  |            ||  | ||
+// || |  |                    -------------  |  |            ||  | ||
+// || |  -------------------------------------  --------------|  | ||
+// || |                                          --------------  | ||
+// || ------------------------------------------------------------ ||
+// |----------------------------------------------------------------|
+// -----------------------------------------------------------------
 class Basilico: public QMainWindow
 {
     // timer
@@ -33,29 +44,39 @@ class Basilico: public QMainWindow
     // Plugins
     std::unordered_map<String, Vector<BasilPlugin*>> Plugins;
 
-    // Integration widget
+    // CentralWidget : Integration of widgets
     QWidget neovim_integrate;
-
+    // Layout for CentralWidget
     QGridLayout basil_layout;
-
-    // Integration neovim tab and plugin tabs
-    QStackedWidget neovim_tabplugins_integrate;
-    int neovim_index; // this must be 0
-    std::map<Tabpage, std::pair<String, int>> TabPluginId;
 
     // Tabs
     QTabBar tabline;
 
+    // Integration of neovim-splitplugin-integ and tab-plugin
+    QStackedWidget neovimsplinteg_tabplugins_integrate;
+    int neovim_index; // this must be 0
+    std::map<Tabpage, std::pair<String, int>> TabPluginId; // tab, [name, stack index]
+    std::multimap<Tabpage, std::tuple<QWidget*, int , int, int, int>> SplitPlugins;
+
     // options
     Integer showtabline;
     String tablineStyleSheet;
-public:
-    String Name = "Basilico";
 
+    QWidget neovim_split_plugins_integrate;
+    QGridLayout neovim_layout;
+
+    // currentTabpage Handler
+    Tabpage currentTabpage;
+
+    const String Name = "Basilico";
     NeoVim neovim;
+
+
+public:
 
     Basilico(String port, uint width, uint height, QApplication& app);
     Basilico() = delete;
+    Basilico operator=(QMainWindow) = delete;
 
     void open();
 
@@ -66,11 +87,32 @@ public:
     void tablineSetting();
     void changeTabNeoVim(const deque<pair<Tabpage, String>>& tabs, Tabpage current);
 
+    void popupmenuSetting();
+
+    const String& getName() const { return Name; }
+    NeoVim& getNeoVim()& { return neovim; }
+
+    QListWidget nvim_comp;
+    QListWidget& get_nvim_comp() { return nvim_comp; };
+
     template<typename T>
     void addTabPluginId(T* plugin, String name, Tabpage tab)
     {
-        auto index = neovim_tabplugins_integrate.addWidget(plugin);
+        auto* integrate = new QWidget{};
+        auto* layout = new QGridLayout{integrate};
+
+        layout->setContentsMargins(0, 0, 0, 0);
+        layout->addWidget(plugin, 1, 0);
+        integrate->setLayout(layout);
+
+        // stackwidget index
+        Integer index = neovimsplinteg_tabplugins_integrate.addWidget(integrate);
         TabPluginId[tab] = std::make_pair(name, index);
+    }
+
+    const std::multimap<Tabpage, std::tuple<QWidget*, int , int, int, int>>& getSplitPlugins() const
+    {
+        return SplitPlugins;
     }
 
     Tabpage makeTabForPlugin(String name);
--- plugin/basilico_autocmds.vim
+++ plugin/basilico_autocmds.vim
@@ -9,5 +9,7 @@ setlocal runtimepath+=.
 let s:save_cpo = &cpo
 set cpo&vim
 
+
+
 let &cpo = s:save_cpo
 unlet s:save_cpo
