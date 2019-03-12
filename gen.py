0846253572515dd29f93a0ac4d21046319a12571
tsubota-kouga <kouga.infini@gmail.com>
2019-03-12 18:51:02 +0900

    np need not socket path

--- gen.py
+++ gen.py
@@ -5,81 +5,77 @@ from glob import glob
 import os, sys, re
 import toml
 
-port: str
-try:
-    # port = sys.argv[1]
-    path = sys.argv[1]
-except:
-    port = '6666'
 
+fill = {}
 this_dir = os.path.dirname(__file__)
 env = Environment(loader=FileSystemLoader(this_dir  + '/template'))
-fill = {}
 
-# nvim = attach('tcp', address='localhost', port=port)
-nvim = attach('socket', path=path)
+# no gui plugins
+if len(sys.argv) >= 3 and sys.argv[2] == 'np':
+    # dependency for Basilico
+    fill['qt_version'] = 5.7
+    fill['boost_version'] = 1.55
+    fill['qt_dependency'] = ['Widgets', 'Core']
+    fill['boost_dependency'] = ['system', 'thread']
 
-basil_file_name = 'basil.toml'
+else:
+    if len(sys.argv) >= 2:
+        path = sys.argv[1]
+    else:
+        print('argment 2: path to socket is required')
+        sys.exit(1)
 
-fill['qt_version'] = 5.7
-fill['boost_version'] = 1.55
-fill['qt_dependency'] = ['Widgets', 'Core']
-fill['boost_dependency'] = ['system', 'thread']
-fill['plugin_paths'] = []
-fill['plugin_name'] = []
-fill['autocmd'] = []
+    nvim = attach('socket', path=path)
+
+    basil_file_name = 'basil.toml'
+
+    fill['qt_version'] = 5.7
+    fill['boost_version'] = 1.55
+    fill['qt_dependency'] = ['Widgets', 'Core']
+    fill['boost_dependency'] = ['system', 'thread']
+    fill['plugin_paths'] = []
+    fill['plugin_name'] = []
+    fill['autocmd'] = []
 
 # array
-runtimepath = nvim.eval('&runtimepath').split(',')
-for path in runtimepath:
-    basil_path = glob(os.path.join(path, basil_file_name))
-    if basil_path != [] and path != '.':
-        head, tail = os.path.split(path)
-        if tail != 'Basilico':
-            fill['plugin_paths'].append(path)
-            fill['plugin_name'].append(tail)
+    runtimepath = nvim.eval('&runtimepath').split(',')
+    for path in runtimepath:
+        basil_path = glob(os.path.join(path, basil_file_name))
+        if basil_path != [] and path != '.':
+            head, tail = os.path.split(path)
+            if tail != 'Basilico':
+                fill['plugin_paths'].append(path)
+                fill['plugin_name'].append(tail)
 
-for p in fill['plugin_paths']:
-    try:
-        f = toml.load(open(os.path.join(p, basil_file_name)))
-    except(FileNotFoundError):
-        continue
+    for p in fill['plugin_paths']:
+        try:
+            f = toml.load(open(os.path.join(p, basil_file_name)))
+        except(FileNotFoundError):
+            continue
 
-    f.setdefault('dependency', {})
-    lang = f['dependency'].setdefault('lang', 'cpp')
-    if lang == 'cpp':
-        fill['qt_dependency'].extend(f['dependency'].setdefault('qt_lib', ''))
-        fill['boost_dependency'].extend(f['dependency'].setdefault('boost_lib', ''))
-        if 'neovim' in f.keys():
-            fill['autocmd'].extend(f['neovim'].setdefault('autocmd', ''))
-        fill['qt_version'] = f['dependency']['qt_version'] \
-                if fill['qt_version'] < f['dependency'].setdefault('qt_version', 0) else fill['qt_version']
-        fill['boost_version'] = f['dependency']['boost_version'] \
-                if fill['boost_version'] < f['dependency'].setdefault('boost_version', 0)\
-                else fill['boost_version']
+        f.setdefault('dependency', {})
+        lang = f['dependency'].setdefault('lang', 'cpp')
+        if lang == 'cpp':
+            fill['qt_dependency'].extend(f['dependency'].setdefault('qt_lib', ''))
+            fill['boost_dependency'].extend(f['dependency'].setdefault('boost_lib', ''))
+            if 'neovim' in f.keys():
+                fill['autocmd'].extend(f['neovim'].setdefault('autocmd', ''))
+            fill['qt_version'] = f['dependency']['qt_version'] \
+                    if fill['qt_version'] < f['dependency'].setdefault('qt_version', 0) else fill['qt_version']
+            fill['boost_version'] = f['dependency']['boost_version'] \
+                    if fill['boost_version'] < f['dependency'].setdefault('boost_version', 0)\
+                    else fill['boost_version']
 
-fill['source_paths'] = [
-    os.path.join(fill['plugin_paths'][i], 'src', fill['plugin_name'][i] + '.cpp')
-    for i in range(len(fill['plugin_name']))]
+    fill['source_paths'] = [
+        os.path.join(fill['plugin_paths'][i], 'src', fill['plugin_name'][i] + '.cpp')
+        for i in range(len(fill['plugin_name']))]
 
-fill['qt_dependency'] = list(set(fill['qt_dependency']))
-fill['boost_dependency'] = list(set(fill['boost_dependency']))
-fill['autocmd'] = list(set(fill['autocmd']))
+    fill['qt_dependency'] = list(set(fill['qt_dependency']))
+    fill['boost_dependency'] = list(set(fill['boost_dependency']))
+    fill['autocmd'] = list(set(fill['autocmd']))
 
 for k in fill.keys():
     print(k, ':', fill[k])
-
-# no gui plugins
-try:
-    if sys.argv[2] == 'np':
-        # dependency for Basilico
-        fill = {}
-        fill['qt_version'] = 5.7
-        fill['boost_version'] = 1.55
-        fill['qt_dependency'] = ['Widgets', 'Core']
-        fill['boost_dependency'] = ['system', 'thread']
-except:
-    pass
 
 template = env.get_template('CMakeLists.txt')
 rendered = template.render(message=fill)
